153eaaaa6a3e1b0bb444df5a4271c88eab2b739e
avvass <amagaji1998@gmail.com>
2024-06-24 21:55:04 +0100

    my changes

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,12 +9,12 @@ export default function Header() {
           </Link>
           <Link href={'/'}>Home</Link>
           <Link href={'/about'}>About</Link>
-          <Link href={'/servic'}>Services</Link>
+          <Link href={'/service'}>Services</Link>
           <Link href={'/contact'}>Contact</Link>
         </nav>
         <nav className="flex items-center gap-4 text-gray-500 font-bold">
-          <Link href=""> Login </Link>
-          <Link href={''} className="bg-red-800 rounded-full text-white px-6 py-2">Register</Link>
+          <Link href="/login"> Login </Link>
+          <Link href={'/register'} className="bg-red-800 rounded-full text-white px-6 py-2">Register</Link>
         </nav>
       </header>
    );
